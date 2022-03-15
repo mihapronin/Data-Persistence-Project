fd5c28a841f6d8dbc5c7feb25aa76cd783b27201
mihapronin <mikhail.pronin@gmail.com>
2022-03-15 22:07:20 +0300

    Some text cleanups

--- Assets/Scripts/TopListUIHandler.cs
+++ Assets/Scripts/TopListUIHandler.cs
@@ -9,13 +9,11 @@ public class TopListUIHandler : MonoBehaviour
     public TextMeshProUGUI topListNames;
     public TextMeshProUGUI topListScore;
 
-    // Start is called before the first frame update
     void Start()
     {
         TopListToScreen();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
